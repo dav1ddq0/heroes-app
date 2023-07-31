9be09791364dbacbdee3a4b23aed62e607f80731
dav1ddq0 <dquesada969x@gmail.com>
2023-07-30 22:34:34 -0400

    added-context-image-path

--- src/components/hero/HeroScreen.jsx
+++ src/components/hero/HeroScreen.jsx
@@ -1,26 +1,32 @@
 import React, { useMemo } from "react";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getHeroesById } from "../../selectors/getHeroeById";
+import { heroImages } from "../../helpers/heroImages";
 
 export const HeroScreen = () => {
   const { heroId } = useParams();
   const navigate = useNavigate();
-  const hero = useMemo(()=>  getHeroesById(heroId), [heroId]);
+  const hero = useMemo(() => getHeroesById(heroId), [heroId]);
 
-  const handleReturn = ()=>{
-    navigate(-1 );
-  }
+  const handleReturn = () => {
+    navigate(-1);
+  };
   if (!hero) {
     return <Navigate to="/" />;
   }
 
   const { id, superhero, publisher, alter_ego, first_appearance, characters } =
     hero;
-  const imagePath = `/assets/${hero.id}.jpg`;
+
+    
   return (
     <div className="row mt-5">
       <div className="col-4 animate__animated animate__fadeInLeft">
-        <img src={imagePath} alt={superhero} className="img-thumbnail" />
+        <img
+          src={heroImages(`./${id}.jpg`)}
+          alt={superhero}
+          className="img-thumbnail"
+        />
       </div>
 
       <div className="col-8">
@@ -46,7 +52,6 @@ export const HeroScreen = () => {
         <h5>Characters</h5>
         <p>{characters}</p>
         <button className="btn btn-outline-info" onClick={handleReturn}>
-        
           Regresar
         </button>
       </div>
